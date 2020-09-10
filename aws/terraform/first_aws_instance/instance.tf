1add67595e9b2c97bf7fbcab40a35f866093957d
kevinp-git <ke7inp@gmail.com>
2020-09-10 17:33:02 -0400

    update dir structure and created example .tf

---
+++ aws/terraform/first_aws_instance/instance.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-06b263d6ceff0b3dd"
+  instance_type = "t2.micro"
+}
